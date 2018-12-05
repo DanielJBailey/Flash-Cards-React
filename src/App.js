11c8788441ede5d5c1dfb5dc098cc5bc4361192c
Daniel Bailey <daniel.justin.bailey@gmail.com>
2018-12-05 15:55:54 -0700

    add form methods

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CardForm from './Components/CardForm';
+import './Styles/App.scss';
 
 class App extends Component {
+  state = {
+    cards: [
+      {id: 1, front: "Question", back: "Answer"},
+      {id: 2, front: "Question2", back: "Answer2"},
+      {id: 3, front: "Question3", back: "Answer3"},
+      {id: 4, front: "Question4", back: "Answer4"},
+      {id: 5, front: "Question5", back: "Answer5"},
+      {id: 6, front: "Question6", back: "Answer6"},
+      {id: 7, front: "Question7", back: "Answer7"},
+      {id: 8, front: "Question8", back: "Answer8"},
+      {id: 9, front: "Question9", back: "Answer9"},
+      {id: 10, front: "Question10", back: "Answer10"}
+    ],
+    menuOpen: false
+  }
+
+  // TOGGLE NEW CARD MENU
+  toggleMenu = () => {
+    let { menuOpen } = this.state
+    this.setState({
+      menuOpen: !menuOpen
+    })
+  }
+  // DELETE CARDS FROM ARRAY
+  deleteCards = () => {
+    let { cards } = this.state
+    this.setState({
+      cards: []
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        {this.state.menuOpen ? <div className="overlay">
+        <CardForm />
+        </div> : null}
+        <div className="container">
+          <button className="btn" onClick={this.toggleMenu}>Add Card</button>
+          <button className="btn" onClick={this.deleteCards}>Delete Cards</button>
+        </div>
+      </>
     );
   }
 }
+
 
 export default App;
---
+++ src/Components/CardForm.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import '../Styles/CardForm.scss';
+
+class CardForm extends React.Component {
+    state = {
+        front: "",
+        back: ""
+    }
+
+    // Handle Form Submit
+    handleSubmit = (e) => {
+        let { front, back } = this.state
+        e.preventDefault();
+        // pass to parent function
+        this.setState({
+            front: "",
+            back: ""
+        })
+    };
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    };
+
+
+    render() {
+        let { front, back } = this.state;
+        return(
+            <form 
+                className="card-form"
+                onSubmit={this.handleSubmit}
+                >
+
+                <textarea
+                    placeholder="Question:"
+                    name="front"
+                    value={front}
+                    className="input"
+                    required
+                    onChange={this.handleChange}
+                />
+                <textarea
+                    placeholder="Answer:"
+                    name="back"
+                    value={back}
+                    onChange={this.handleChange}
+                    className="input"
+                    required
+                />
+                <input 
+                    type="submit"
+                    value="Submit"
+                    className="submit" 
+                />
+            </form>
+        )
+    }
+}
+
+
+export default CardForm;
