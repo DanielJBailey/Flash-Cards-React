43f92a15f745e2d7a1f92cd49789fb80768b23ad
Daniel Bailey <daniel.justin.bailey@gmail.com>
2018-12-06 20:18:20 -0700

    remove function

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -35,13 +35,6 @@ class Home extends React.Component {
     })
   }
 
-  changeFrontAndBack = (front, back) => {
-    this.setState({
-      front: front,
-      back: back
-    })
-  }
-
   // TOGGLE EDITING MODE
   toggleEdit = () => {
     let {cards, currentCard } = this.state
@@ -53,7 +46,6 @@ class Home extends React.Component {
     });
   }
  
-
   // DELETE ALL CARDS FROM ARRAY
   deleteCards = () => {
     this.setState({
@@ -133,10 +125,10 @@ class Home extends React.Component {
   }
 
   render() {
-    let { cards, currentCard, flipped, front, back, editing } = this.state;
+    let { cards, currentCard, flipped, front, back, editing, menuOpen } = this.state;
     return (
       <>
-        {this.state.menuOpen ? <div className="overlay">
+        {menuOpen ? <div className="overlay">
           <div className="close-menu" onClick={this.toggleMenu}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z" /></svg>
           </div>
@@ -149,7 +141,7 @@ class Home extends React.Component {
             handleChange={this.handleChange}
           />
         </div> :
-          (this.state.editing) ?
+          (editing) ?
             <div className="overlay">
               <div className="close-menu" onClick = {this.toggleEdit}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z" /></svg>
